4e76eb43a393accce78bc06632958c561535f72f
Nikita Miroshnichenko <nikitamiroshnichenko@coherentsolutions.com>
2022-05-19 01:47:25 +0200

    changed activity naming

--- app/src/main/java/com/mirosha/catsgenerator/CatsGeneratorActivity.kt
+++ app/src/main/java/com/mirosha/catsgenerator/CatsGeneratorActivity.kt
@@ -1,11 +1,11 @@
 package com.mirosha.catsgenerator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class CatsGeneratorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cats_generator)
     }
 }
