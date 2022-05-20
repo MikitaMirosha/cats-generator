37afed99c6806c7d4a2a9f6ca5fa35e98ce8b75d
Nikita Miroshnichenko <nikitamiroshnichenko@coherentsolutions.com>
2022-05-21 01:36:49 +0200

    added repository

---
+++ app/src/main/java/com/mirosha/catsgenerator/repo/CatRepository.kt
@@ -0,0 +1,25 @@
+package com.mirosha.catsgenerator.repo
+
+import com.mirosha.catsgenerator.base.BaseApiResponse
+import com.mirosha.catsgenerator.data.remote.RemoteCatDataSource
+import com.mirosha.catsgenerator.model.CatResponse
+import com.mirosha.catsgenerator.utils.NetworkStatus
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+class CatRepository
+@Inject constructor(
+    private val remoteCatDataSource: RemoteCatDataSource
+) : BaseApiResponse() {
+
+    suspend fun getRandomCat(): Flow<NetworkStatus<CatResponse>> {
+        return flow {
+            emit(makeApiCall { remoteCatDataSource.getRandomCat() })
+        }.flowOn(Dispatchers.IO)
+    }
+}
